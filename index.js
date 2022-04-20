0e56a9a28f4f2ab7702752247b24467a5f86cd54
atmason90 <atmason90@gmail.com>
2022-04-19 21:33:52 -0700

    add questions array

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe this project.',
+    },
+    {
+        type: 'input',
+        name: 'instructions',
+        message: 'What are the instalation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'usageInfo',
+        message: 'Enter the usage information.',
+    },
+    {
+        type: 'input',
+        name: 'contributionGuidelines',
+        message: 'What are the contribution guidelines?',
+    },
+    {
+        type: 'input',
+        name: 'testInstructions',
+        message: 'What are the test instructions?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license would you like to use?',
+        choices: ['MIT License', 'Apache License 2.0', 'Mozilla Public License 2.0', 'GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Boost Software License 1.0', 'The Unilicense'],
+        default: '0',
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
